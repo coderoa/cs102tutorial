e567a352b9fe90eb5183b322e120a31c9bfd2445
CS102 tutorial <250458@newuu.uz>
2025-09-09 12:42:01 +0500

    Problem 1-8

---
+++ main.cpp
@@ -0,0 +1,76 @@
+// Week 3,
+
+#include  <iostream>
+using namespace std;
+
+//Problem 1
+// int main() {
+//     int intNumber = 20;
+//     float floatNumber = 3.14;
+//     double doubleNumber = 45.1234;
+//     bool boolean = true;
+//     char charName = 'A';
+//
+//     cout << "Value of Integer is " <<intNumber<<". Size is " <<sizeof(intNumber)<<endl;
+//     cout << "Value of Float is " <<floatNumber<<". Size is " << sizeof(floatNumber)<<endl;
+//     cout << "Value of Double is " <<doubleNumber<<". Size is " << sizeof(doubleNumber)<<endl;
+//     cout << "Value of Boolean is " <<boolean<<". Size is " << sizeof(boolean)<<endl;
+//     cout << "Value of Character is " <<charName<<". Size is " << sizeof(charName)<<endl;
+// }
+
+//Problem 2
+// int main() {
+//     cout<<"Ozodbek Azimurodov\n250458\nQueen - The Show must Go On\nTitanic"<<endl;
+//     return 0;
+// }
+
+//Problem 3
+// int main() {
+//     cout<< ("\"I'm standing\" on the edge of some crazy cliff.\n"
+//             "What I have to do, I have to catch everybody\\\n"
+//             "\t\\if they're runnign and tey don't look"
+//             "\twhere they're going  I have to come out\n\tfrom somewhere and  \tchatch them.\n"
+//             "Refer to escape seequences table");
+//     return 0;
+// }
+
+//Problem 4
+// int main() {
+//     int a = 189;
+//     char b = 'B';
+//     char c = a/10-1+b;
+//     cout << a/3.0<< " " << toascii(b) << " " <<(a+b)/5<< " "<< c;
+// }
+
+//Problem 5
+// int main() {
+//     int num1 = 44;
+//     int num2 = 12;
+//     cout <<"Sum=" <<num1+num2 << endl;
+//     cout <<"Product="<<num1*num2 << endl;
+//     return 0;
+// }
+
+//Problem 6
+// int main() {
+//     int a,b;
+//     cin>>a>>b;
+//     cout<<"Sum:"<<a+b<<endl;
+//     return 0;
+// }
+
+//Problem 7
+// int main() {
+//     float x,y;
+//     cin>>x>>y;
+//     float t=(3*(x*x+3))/((y/4)+3);
+//     cout<<"t="<<t<<endl;
+//     return 0;
+// }
+
+//Problem 8
+// int main() {
+//     float radius;
+//     cin >> radius;
+//     cout<<"Area: "<<radius*radius*3.14<<endl;
+// }
