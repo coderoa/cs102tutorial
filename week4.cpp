c5d086e207d4ea90ba5679f8c74117316a1dbe84
CS102 tutorial <250458@newuu.uz>
2025-09-16 12:36:54 +0500

    Week 3

---
+++ week4.cpp
@@ -0,0 +1,147 @@
+//
+// Created by Ozodbek Azimurodov on 16/09/25.
+//
+
+
+//Week 4
+
+#include <iostream>
+using namespace std;
+
+//Problem 1
+// int main() {
+//     int num;
+//     cin>>num;
+//     if (num>0) {
+//         cout<<"The number is positive"<<endl;
+//     }else {
+//         cout<<"The number is negative"<<endl;
+//     }
+// }
+
+//Problem 2
+// int main() {
+//     int num;
+//     cin>>num;
+//     if (num%2==0) {
+//         cout<<"Even"<<endl;
+//     }else {
+//         cout<<"Odd"<<endl;
+//     }
+// }
+
+//Problem 3
+// int main() {
+//     int a, b;
+//     cin>>a>>b;
+//     if (a>b) {
+//         cout<<a;
+//     }else {
+//         if (b>a) {
+//             cout<<b;
+//         }else {
+//             cout<<a<<"="<<b;
+//         }
+//     }
+// }
+
+
+
+//Problem 4
+// int main() {
+//     int a, b, c;
+//     cin>>a>>b>>c;
+//     if (a>b){
+//         if (a>c) {
+//             cout<<a<<endl;
+//         }else {
+//             cout<<c<<endl;
+//         }
+//     }else {
+//         if (b>c) {
+//             cout<<b<<endl;
+//         }
+//         else {
+//             cout<<c<<endl;
+//         }
+//     }
+// }
+
+
+//Problem 5
+// int main() {
+//     int y;
+//     cin>>y;
+//     if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
+//         cout<<"Leap year"<<endl;
+//     }else {
+//         cout<<"Not a Leap year"<<endl;
+//     }
+// }
+
+//Problem 6
+// int main() {
+//     int speed;
+//     cin>>speed;
+//     if (speed<20) {
+//         cout<<"Too slow";
+//     }else if(speed>80){
+//     cout<<"Too fast";}
+//     else {
+//         cout<<"Just right";
+//     }
+//     return 0;
+// }
+
+//Problem 7
+// int main() {
+//     int score;
+//     cin>>score;
+//     if (score>=50) {
+//         cout<<"Pass";
+//     }
+//     else {
+//         cout<<"Fail";
+//     }
+// }
+
+//Problem 8
+// int main() {
+//     int a;
+//     cin>>a;
+//     if (a>0) {
+//         cout<<"Positive";
+//     }else {
+//         if (a<0) {
+//             cout<<"Negative";
+//         }
+//         else {
+//             cout<<"Zero";
+//         }
+//     }
+//     return 0;
+// }
+
+//Problem 9
+// int main() {
+//     char l;
+//     cin>>l;
+//     switch (l) {
+//         case 'g':
+//             cout<<"GO";
+//             break;
+//         case 'y':
+//             cout<<"Get ready!";
+//             break;
+//         case 'r':
+//             cout<<"Stop";
+//             break;
+//         default:
+//             cout<<"wrong input";
+//     }
+// }
+
+//Problem 10
+int main() {
+
+}
